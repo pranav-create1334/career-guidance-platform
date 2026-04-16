dfd31a962dce09a4431bf4c3a286e4a4f7033a49
Pranav <pranavkusalkar39@gmail.com>
2026-04-16 23:45:46 +0530

    feat: integrate Spring Security with JWT authentication

---
+++ user-service/src/main/java/com/career/user_service/security/JwtUtil.java
@@ -0,0 +1,22 @@
+package com.career.user_service.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public static String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+}
